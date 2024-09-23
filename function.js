4afc4563a71a1dca16055f4243846384f5c0a193
Sadman kabir <sadmankabir351@gmail.com>
2024-09-24 05:26:21 +0600

    donation section responsive

--- function.js
+++ function.js
@@ -1,1 +1,7 @@
-console.log('ami tmk valobashi')
+document.getElementById('btn-donate-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+})
